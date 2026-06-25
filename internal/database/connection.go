6f78511bfd6a4ca3924d94237a614f456cea326f
Giovanni Ferron <info@gvnn.it>
2026-06-25 13:14:27 +1000

    feat: Add connection utility (#3)

---
+++ internal/database/connection.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func NewConnection(driver string, dsn string) (*gorm.DB, error) {
+	var db *gorm.DB
+	var err error
+
+	switch driver {
+	case "sqlite":
+		db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+
+	default:
+		return nil, fmt.Errorf("unsupported database driver: %s", driver)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
